df5d12aad87e0589f6d83925cbb8c9034f44d860
ale89 <ale.mariotti@gmail.com>
2013-03-06 23:43:11 +0100

    aggiunti script

---
+++ learning-javascript/2013-03-05/exercise1.js
@@ -0,0 +1,12 @@
+console.log("Esercizio 1");
+
+var mat = "";
+
+for (var i=1; i<=10; i++) {
+	for (var j=1; j<=10; j++) {
+		mat += (i*j) + "\t";
+        }
+	mat += "\n";
+}
+
+console.log(mat); 
---
+++ learning-javascript/2013-03-05/exercise2.js
@@ -0,0 +1,11 @@
+console.log("Esercizio 2");
+
+var mat ="";
+for (var i=1; i<=10; i++) {
+	for (var j=1; j<10; j++) {
+		mat += (i*j) + "," +"\t";
+        }
+	mat += (i*j) +"\n";
+}
+
+console.log(mat); 
